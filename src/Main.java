0d8d600f97192fec5d761e29c7ef7ee34586b851
Дмитрий <trololodim69@gmail.com>
2023-11-09 19:46:11 +0300

    hello2

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,11 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+
+
+
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
